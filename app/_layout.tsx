867117152d15f7f7a5e88f698786b8bd0ef62cd0
michaeliryami <miryamipro@gmail.com>
2025-05-07 00:06:00 -0400

    Add files via upload

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaProvider>
+    </ClerkProvider>
+  );
+}
